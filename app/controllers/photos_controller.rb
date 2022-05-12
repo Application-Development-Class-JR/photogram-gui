62db68a0f6c8f89d2f4d3e2abe34ad4cad4f5a7f
juliannaresende <jresende@chicagobooth.edu>
2022-05-12 06:23:07 +0000

    Erro no add photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,50 @@
 class PhotosController < ApplicationController
 
   def index
+
+    matching_photos = Photo.all
+    @list_of_photos=  matching_photos.order({ :created_at => :desc})
+    
   render ({ :template => "photos_templates/index.html.erb" })
   end
+
+  def show
+    url_photo_id = params.fetch("path_photo_id")
+    matching_photo_id = Photo.where({:id => url_photo_id})
+    @the_photo =  matching_photo_id.at(0)
+    if @the_photo == nil
+      redirect_to("/404")
+    else
+      render ({ :template => "photos_templates/show.html.erb" })
+    end
+  end
+
+  def delete
+    the_photo_id = params.fetch("path_photo_id")
+    matching_photo_id = Photo.where({ :id => the_photo_id })
+    the_photo = matching_photo_id.at(0)
+     the_photo.destroy
+
+   # render ({ :template => "photos_templates/delete.html.erb" })
+   redirect_to ("/photos")
+  end
+
+  def create
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+    input_owner_id = params.fetch("query_owner_id")
+
+    a_new_photo = Photo.new
+
+    a_new_photo.image = input_image
+    a_new_photo.caption = input_caption
+    a_new_photo.owner_id = input_owner_id
+
+    a_new_photo.save
+
+    #render ({ :template => "photos_templates/create.html.erb" })
+    redirect_to("/photos/" + a_new_photo.id.to_s)
+  end
+
 end
  
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   get("/users/:path_username", { :controller => "users", :action => "show"})
 
   get("/photos", { :controller => "photos", :action => "index"})
+  get("/photos/:path_photo_id", { :controller => "photos", :action => "show"})
+
+  get("/delete_photo/:path_photo_id", { :controller => "photos", :action => "delete"})
+
+  get("/insert_photo_record", { :controller => "photos", :action => "create"})
+
 end
